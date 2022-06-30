b1bae7142faecd79cd1f2660a10247a646fef508
alceoritter <alceoritter@gmail.com>
2022-06-30 19:47:37 +0700

    migration,modal,controller artikel

---
+++ database/migrations/2022_06_30_124459_create_artikel_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtikelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikel', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('id_user');
+            $table->string('title');
+            $table->text('detail');
+            $table->text('url_file');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikel');
+    }
+}
